e6b9d9e85ab833d40d0e370e871e256952463899
Daniel <daniel7.oliveira@gmail.com>
2018-08-08 16:31:51 -0300

    fiz o exercicio 1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int soma = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+    }
+}
